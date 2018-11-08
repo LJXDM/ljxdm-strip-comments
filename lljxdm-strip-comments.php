4a1629b08054cbd6a630c4a939a732993a522d3f
ac <ac@ljx.uk>
2018-11-08 12:51:46 +0000

    init

---
+++ lljxdm-strip-comments.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Plugin Name: LJXDM Strip HTML comments
+ * Plugin URI: https://github.com/LJXDM/ljxdm-strip-comments
+ * Description: Strip all html code comments from your Wordpress site
+ * Version: 0.1
+ * Author: ljxdm
+ * Author URI: https://lojinx.digital
+ * License: GPLv3 or later
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+
+class ljxdmStripComments
+{
+	public function __construct() {
+		add_action('get_header', array($this, 'start'), 100);
+		add_action('wp_footer', array($this, 'end'), 100);
+	}
+
+	private static function strip($html) {
+		return preg_replace('/<!--(.|\s)*?-->/','',$html);
+	}
+
+	public static function start() {
+		ob_start(array($this, 'strip'));
+	}
+
+	public static function end() {
+		ob_end_flush();
+	}
+}
+
+// PHP 5.4 required
+new ljxdmStripComments;
+
+?>
